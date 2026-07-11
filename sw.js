6e5359ec22bc85c4f5b6709ee0d45b02723a8d78
naptech2212 <mail.naptech@gmail.com>
2026-07-11 12:21:32 +0530

    Update sw.js
    version updated

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// Fleet Compliance Tracker — Service Worker v3.0
-const CACHE = 'fct-v3.0';
+// Fleet Compliance Tracker — Service Worker v3.5
+const CACHE = 'fct-v3.5';
 const ASSETS = [
   './', 
   './index.html', 
